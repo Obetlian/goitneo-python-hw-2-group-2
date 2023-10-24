6ca704426f7045bb32ecc86e4675751088c44323
Obetlian <59937999+Obetlian@users.noreply.github.com>
2023-10-23 20:24:40 -0400

    Add files via upload

---
+++ addressbook.py
@@ -0,0 +1,57 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if not str(value).isdigit() or len(str(value)) != 10:
+            raise ValueError("Phone number should have 10 digits.")
+        self.value = value
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                break
+
+    def edit_phone(self, old_phone, new_phone):
+        for idx, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[idx] = Phone(new_phone)
+                break
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
